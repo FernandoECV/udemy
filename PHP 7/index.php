9d5d31fc53b12f1cad0ae049e5976f1719ba1a41
Fernando Santos <fernandop.santossa@gmail.com>
2019-01-05 01:22:05 -0200

    Inserindo projeto no github

---
+++ PHP 7/index.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+
+	<title>Curso PHP</title>
+
+</head>
+
+
+<body>
+		
+		<?php
+
+			echo "utilizando a tag padrão";
+
+		?>
+
+		<br />  
+
+		<?= 'Utilizando a tag impressão' ?>
+
+
+
+</body>
+
+
+
+</html>

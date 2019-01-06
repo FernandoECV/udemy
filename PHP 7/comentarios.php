992bf87458b3f148ffced37d2e1dca8be9906e9e
Fernando Santos <fernandop.santossa@gmail.com>
2019-01-06 02:57:57 -0200

    Comentários em PHP

---
+++ PHP 7/comentarios.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+
+	<title>Curso PHP</title>
+
+</head>
+
+
+<body>
+
+		<!-- Comentário em HTML -->
+		
+		<?php
+
+			// Comentario de uma linha
+
+			echo "Comando echo<br/>";
+
+			# Comentário de uma linha 
+
+			print ("Comando print<hr/>")
+
+			/*
+
+			Comentário de múltiplas linhas 
+
+			*/
+
+		?>
+
+
+
+</body> 
+
+
+
+</html>

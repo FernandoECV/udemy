40dcf3a3606f36aa8fe18b33379af83f7740b52f
Fernando Santos <fernandop.santossa@gmail.com>
2019-01-06 03:02:29 -0200

    Variáveis em PHP

---
+++ PHP 7/variaveis.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+
+	<title>Curso PHP</title>
+
+</head>
+
+
+<body>
+		
+		<?php
+
+			// string
+
+			$nome = "Fernando Santos";
+
+			// int
+
+			$idade = 29;
+
+			// float
+
+			$peso = 82.5;		
+
+			// boolean 
+
+			$fumante_sn = true;	// (true = 1) ou (false = 0)
+
+		?>
+
+		<h1>Ficha cadastral</h1>
+		<br/>
+		<p>Nome:  <?= $nome ?> </p>
+		<p>Idade: <?= $idade ?> </p>
+		<p>Peso:  <?= $peso ?> </p>
+		<p>Fumante:  <?= $fumante_sn ?> </p>
+
+
+
+</body> 
+
+
+
+</html>

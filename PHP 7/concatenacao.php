e5b44ce759a4465b0e6d9d738d42ad28cca1af3b
Fernando Santos <fernandop.santossa@gmail.com>
2019-01-08 02:07:25 -0200

    Concatenação em PHP

---
+++ PHP 7/concatenacao.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+
+	<title>Curso PHP</title>
+
+</head>
+	
+
+<body>
+		
+		<?php
+
+
+			$nome = 'Fernando';
+			$cor = 'azul';
+			$idade = 25;
+			$atividade_preferida = 'Jogar free fire';
+			
+
+			// operador . 
+
+				
+			echo ' Olá '. $nome .', vi que sua cor preferida é '. $cor .' , estou vendo também que você possui '. $idade .' anos e que gosta de jogar '. $atividade_preferida .'. ';
+
+			// aspas duplas
+
+			echo '<br><br>';
+
+
+			echo "Olá $nome, vi que sua cor preferida é $cor, estou vendo também que você possui $idade anos e que gosta de jogar $atividade_preferida.";	
+
+
+		?>
+
+		
+
+
+
+</body> 
+
+
+
+</html>

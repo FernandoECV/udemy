617ef4d5e40326e92541f11cb8bc87293b564fbb
Fernando Santos <fernandop.santossa@gmail.com>
2019-01-08 23:11:08 -0200

    if e else em PHP

---
+++ PHP 7/ifelse_operadores_condicionais.php
@@ -0,0 +1,54 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+
+	<title>Curso PHP</title>
+
+</head>
+	
+
+<body>
+		
+		<?php
+
+			// == operador de igualdade
+			// === operador de idêntico 
+			// != ou <> operador de diferença 
+			// !== operador não-idêntico 
+			// > operador de maior quê
+			// < operador de menor quê
+			// <= operador menor igual
+			// >= operador maior igual
+
+		  /*if(25 >= 25) {
+
+				echo "Verdadeiro";
+
+			} else {
+
+				echo "Falso";
+
+
+			}
+				*/
+
+			if ('a' != 'a') {
+
+				echo "Verdadeiro";
+				echo "Segunda instrução<br/><br/>";
+				echo "Terceira instrução";
+				
+				}
+
+		?>
+
+		
+
+
+
+</body> 
+
+
+
+</html>

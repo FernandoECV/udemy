d731c5e4e94f40c4c9b1b2898d240b70c16510b7
Fernando Santos <fernandop.santossa@gmail.com>
2019-01-10 23:26:55 -0200

    if e else em PHP

---
+++ PHP 7/ifelse_operadores_logicos.php
@@ -0,0 +1,109 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+
+	<title>Curso PHP</title>
+
+</head>
+
+
+<body>
+
+	<?php
+
+			// == operador de igualdade
+			// === operador de idêntico 
+			// != ou <> operador de diferença 
+			// !== operador não-idêntico 
+			// > operador de maior quê
+			// < operador de menor quê
+			// <= operador menor igual
+			// >= operador maior igual
+
+			// ** Operadores condicionais/comparação // 
+
+
+
+		// operadores lógicos
+
+		// E: && ou AND -> retorna verdadeiro se todos os resultados das expressões forem verdadeiros 
+
+		// OU: || Ou OR -> retorna verdadeiro se pelo menos um dos resultados da expressões for verdadeiro
+
+		// XOR: XOR -> retorna verdadeiro se uma das expressões for verdadeira e a outra falsa, ou vice-versa 
+
+		// ! -> Inverte o resultado retornado pela expressão 
+
+
+		/* if (5 == 3 && 10 > 3) {
+
+			echo "Verdadeiro";	
+
+		} else {
+
+			echo "Falso";
+
+		}
+
+		*/
+
+		/* if (4 == 4 || 10 > 15) {
+
+			echo "Verdadeiro";	
+
+		} else {
+
+			echo "Falso";
+
+		}
+
+		*/
+
+		/* if (4 == 4 XOR 10 > 15) {
+
+			echo "Verdadeiro";	
+
+		} else {
+
+			echo "Falso";
+
+		}
+
+		*/
+
+		/* if (!('a' == 'b')) {
+
+			echo "Verdadeiro";	
+
+		} else {
+
+			echo "Falso";
+
+		}
+
+		*/
+
+		if (22 == 22 && 3 == 3 && 5 != 5) {
+
+			echo "Verdadeiro";	
+
+		} else {
+
+			echo "Falso";
+
+		}
+
+
+
+	?>
+
+
+
+
+
+</body> 
+
+
+
+</html>

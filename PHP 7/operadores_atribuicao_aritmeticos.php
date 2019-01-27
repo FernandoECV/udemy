677f83ab400c04e140d9394430fcba9c9d86cd5a
Fernando Santos <fernandop.santossa@gmail.com>
2019-01-27 20:31:18 -0200

    Operadores aritméticos com atribuição em PHP

---
+++ PHP 7/operadores_atribuicao_aritmeticos.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+
+	<title>Curso PHP</title>
+
+</head>
+
+
+<body>
+	
+
+	<?php
+
+		$x = 10;
+		$y = 8;
+
+		$x %= $y;
+
+		echo $x;	
+	
+	?>
+
+	
+
+</body> 
+
+
+
+</html>

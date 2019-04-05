1c6ba890193293d81532911c1d7ff140b53c450e
Fernando Santos <fernandop.santossa@gmail.com>
2019-04-05 20:13:45 -0300

    Funções para tarefas matemáticas

---
+++ PHP 7/funcoes_matematicas.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+	<meta charset="utf-8" />
+
+	<title>Curso PHP</title>
+
+</head>
+
+
+<body>
+	
+	<?php
+	
+		 // $num = 7.499999; // 0. e .4 => baixo / .5 => cima 
+
+		// echo ceil($num); // arendonda valor para cima 
+
+		// echo floor($num); 	// arendonda valor para baixo	
+
+		// echo round($num)  // arendonda com base na fração 
+
+		// echo rand(10,20); // gerar um valor aleatório 0 até randmax
+
+		// echo '<br />'.getrandmax();
+
+		echo sqrt(9); // retorna a raiz quadrada de um valor passado por parâmetro 
+
+	?>
+
+	
+
+</body> 
+
+ 
+
+</html>
